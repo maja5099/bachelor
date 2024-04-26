3f1f65d245997e573ffe21420a3ba77f369d9f36
maja5099 <maja5099@stud.kea.dk>
2024-04-26 15:50:13 +0200

    Simple Bottle templating works

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 # 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
 # https://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@github.com/maja5099/bachelor.git
 
-from bottle import default_app, get, post, run
+
+from bottle import default_app, post, route, run, template, static_file, get
 import git
+
  
+##############################
+#   Git and Pythonanywhere 
+
 @post('/secret_url_for_git_hook')
 def git_update():
   repo = git.Repo('./bachelor')
@@ -14,14 +19,21 @@ def git_update():
  
  
 ##############################
-@get("/")
-def _():
-  return "Three"
+#   Routes
+
+@route("/")
+def index():
+   return template('index')
+
+@route("/profile")
+def profile():
+   return template('profile')
  
+
 ##############################
 try:
   import production
   application = default_app()
 except Exception as ex:
   print("Running local server")
-  run(host="127.0.0.1", port=80, debug=True, reloader=True)
+  run(host="127.0.0.1", port=5500, debug=True, reloader=True)
