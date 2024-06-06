88112783fc708f7eca00a426eefcc5c402e4d566
maja5099 <maja5099@stud.kea.dk>
2024-06-06 12:52:48 +0200

    Fine tuned portfolio, services and prices, contact and about us

--- routers/services_and_prices.py
+++ routers/services_and_prices.py
@@ -1,43 +1,90 @@
-from bottle import template, get, request, response
+##############################
+#   IMPORTS
+#   Library imports
+from bottle import template, get, request
+import logging
+import os
+
+#   Local application imports
+from colored_logging import setup_logger
 import master
 import content
-import os
-import traceback
+
 
 ##############################
-#   Content from content.py
-header_nav_items = content.header_nav_items
-footer_info = content.footer_info
-unid_logo = content.unid_logo
-selling_points = content.selling_points
-social_media = content.social_media
-ui_icons = content.ui_icons
-pricing_default = content.pricing_default
-pricing_accent = content.pricing_accent
-section_profile_admin = content.section_profile_admin
-section_profile_customer = content.section_profile_customer
+#   COLORED LOGGING
+try:
+    logger = setup_logger(__name__, level=logging.INFO)
+    logger.setLevel(logging.INFO)
+    logger.success("Logging imported successfully.")
+except Exception as e:
+    logger.error("Error importing logging: %s", e)
+finally:
+    logger.info("Logging import process completed.")
 
 
+##############################
+#   CONTENT VARIABLES
+try:
+    # Global
+    ui_icons = content.ui_icons
+    unid_logo = content.unid_logo
+    # Header
+    header_nav_items = content.header_nav_items
+    selling_points = content.selling_points
+    # Footer
+    footer_info = content.footer_info
+    social_media = content.social_media
+    # Content for this page
+    pricing_default = content.pricing_default
+    pricing_accent = content.pricing_accent
+    logger.success("Content imported successfully.")
+except Exception as e:
+    logger.error("Error importing content: %s", e)
+finally:
+    logger.info("Content import process completed.")
+
+
+##############################
+#   SERVICES_AND_PRICES
 @get("/services_and_prices")
-def _():
+def services_and_prices():
+
+    page_name = "services_and_prices"
+
     try:
-        user = request.get_cookie("user", secret=os.getenv('MY_SECRET'))
-        if user: 
+        # Securely retrieve user cookie
+        user_cookie = request.get_cookie("user", secret=os.getenv('MY_SECRET'))
+
+        # Validate cookie, then fetch user details from db
+        if user_cookie and isinstance(user_cookie, dict):
             db = master.db()
-            print("Type of user:", type(user))
-            print("User:", user)
-            username = user['username']
-            first_name = user['first_name']
-            last_name = user['last_name']
+            username = user_cookie.get('username')
             user = db.execute("SELECT * FROM users WHERE username = ? LIMIT 1", (username,)).fetchone()
+            logger.success("Valid user cookie found for /%s, retrieved data from database", page_name)
+
+        # Handle scenarios where no valid cookie is found (e.g., user not logged in)
         else:
-            user = None
-            username = None
-            first_name = None
-            last_name = None
-        return template('services_and_prices.html', title="UNID Studio - Services og priser", user=user, pricing_default=pricing_default, pricing_accent=pricing_accent, section_profile_admin=section_profile_admin, section_profile_customer=section_profile_customer, first_name=first_name, last_name=last_name, username=username, header_nav_items=header_nav_items, footer_info=footer_info, unid_logo=unid_logo, selling_points=selling_points, social_media=social_media, ui_icons=ui_icons)
+            user = username = None
+            logger.warning("No valid user cookie found for /%s, perhaps user is not logged in yet", page_name)
+
+        return template(page_name, 
+                        title="UNID Studio - Services og priser", 
+                        footer_info=footer_info, 
+                        header_nav_items=header_nav_items, 
+                        pricing_default = pricing_default,
+                        pricing_accent = pricing_accent,
+                        selling_points=selling_points, 
+                        social_media=social_media, 
+                        ui_icons=ui_icons,
+                        unid_logo=unid_logo, 
+                        user=user, 
+                        username=username
+                        )
+    
     except Exception as e:
-        error_message = str(e)
-        traceback_message = traceback.format_exc()
-        response.status = 500
-        return f"<h1>Internal Server Error</h1><p>{error_message}</p><pre>{traceback_message}</pre>"
+        logger.error(f"Error during request for /{page_name}: {e}")
+        raise
+    
+    finally:
+        logger.info(f"Completed request for /{page_name}")
