24bb689e96b8205997166e44d5ea084fc404e5ea
“isab554b” <isab554b@stud.kea.dk>
2024-06-07 17:29:53 +0200

    code documentation to js

--- static/script.js
+++ static/script.js
@@ -16,6 +16,7 @@
 
 // ##############################
 // PROFILE.HTML
+
 document.addEventListener("DOMContentLoaded", function () {
   // Button style and dynamic templates
   const buttons = document.querySelectorAll(".secondary_button");
@@ -97,7 +98,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // ##############################
 // SIGNUP.HTML
+
+// Handles user signup form submission and processing.
 async function signUp() {
+  // Retrieve user input values from the signup form
   const first_name = document.querySelector("input[name='first_name']").value;
   const last_name = document.querySelector("input[name='last_name']").value;
   const email = document.querySelector("input[name='email']").value;
@@ -111,6 +115,7 @@ async function signUp() {
 
   console.log("this is the username", username);
 
+  // Create a FormData object to store form data and append user input values to the FormData object
   const formData = new FormData();
 
   formData.append("first_name", first_name);
@@ -126,15 +131,20 @@ async function signUp() {
     console.log(value);
   }
 
+  // Send a POST request to the server with the form data
   const response = await fetch("/signup", {
     method: "POST",
     body: formData,
   });
+
+  // Parse the response JSON data
   const data = await response.json();
-  
+
+  // If the response contains an error, display it on the signup form
   if (data.error) {
     document.getElementById("error_message").innerText = data.error;
     document.getElementById("form_input_error_message").style.display = "flex";
+    // If signup is successful, log a success message and redirect to the homepage
   } else {
     console.log("Signup successful");
     window.location.href = "/";
@@ -143,24 +153,33 @@ async function signUp() {
 
 // ##############################
 // LOGIN.HTML
+
+// Handles form submission and processing for user login.
 async function login(event) {
   event.preventDefault();
+  // Retrieves the username and password entered by the user
   const username = document.querySelector("input[name='username']").value;
   const password = document.querySelector("input[name='password']").value;
 
+  // Creates a FormData object and appends the username and password to it
   const formData = new FormData();
   formData.append("username", username);
   formData.append("password", password);
 
+  // Sends a POST request to the server with the login credentials
   const response = await fetch("/login", {
     method: "POST",
     body: formData,
   });
+
+  // Parses the response JSON data
   const data = await response.json();
 
+  // If the response contains an error, display it on the login form
   if (data.error) {
     document.getElementById("error_message").innerText = data.error;
     document.getElementById("form_input_error_message").style.display = "flex";
+    // If login is successful, log a success message and redirect to the homepage
   } else {
     console.log("Login successful");
     window.location.href = "/";
@@ -169,10 +188,17 @@ async function login(event) {
 
 // ##############################
 // CUSTOMER_MESSAGES.HTML
+
+// Manages sending messages from the customer side.
 $(document).ready(function () {
+  // Attaches a click event handler
   $("body").on("click", "#sendMessageButton", function () {
     console.log("Send button clicked");
+
+    // Retrieves form data from the contact form
     var formData = new FormData($("#contactForm")[0]);
+
+    // Sends an AJAX request to the server to send the messag
     $.ajax({
       url: "/send_message",
       type: "POST",
@@ -184,6 +210,7 @@ $(document).ready(function () {
         $("#messageSent").text(response.info).show();
         $("#contactForm")[0].reset();
       },
+      // Callback function executed when the request fails
       error: function (xhr) {
         console.error("Error response:", xhr);
         var response = JSON.parse(xhr.responseText);
@@ -195,16 +222,22 @@ $(document).ready(function () {
 
 // ##############################
 // CUSTOMER_CLIPCARDS.HTML
+
+// Handles buying clipcards by customers.
 document.addEventListener("DOMContentLoaded", function () {
+  // Redirects the user to the appropriate URL to buy the clipcard
   function buyClipcard(clipcardType, clipcardPrice) {
     window.location.href =
       "/buy_clipcard/" + clipcardType + "/" + clipcardPrice;
   }
 
+  // Event listener for click events and checks if the clicked element has the right class
   document.addEventListener("click", function (event) {
     if (event.target && event.target.classList.contains("buy-button")) {
+      // Retrieves the clipcard type and price from the clicked button's data attributes
       var clipcardType = event.target.getAttribute("data-clipcard-type");
       var clipcardPrice = event.target.getAttribute("data-clipcard-price");
+      // Initiates the purchase process by calling the buyClipcard function
       buyClipcard(clipcardType, clipcardPrice);
     }
   });
@@ -212,11 +245,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
 // ##############################
 // ADMIN_MESSAGES.HTML
+
+// Handles deleting messages from the admin interface.
 function deleteMessage(button) {
+  // Retrieve the message ID from the button's data attribute
   var messageId = button.getAttribute("data-message-id");
   console.log("Delete button clicked");
   console.log("Message ID:", messageId);
 
+  // Send a DELETE request to the server to delete the message
   fetch("/delete_message", {
     method: "DELETE",
     headers: {
@@ -230,10 +267,12 @@ function deleteMessage(button) {
     })
     .then((data) => {
       console.log("Response data:", data);
+      // If the message was successfully deleted, remove it from the DOM
       if (data.info === "Message deleted.") {
         var messageDiv = button.closest("div");
         messageDiv.parentNode.removeChild(messageDiv);
         console.log("Message deleted successfully.");
+        // If an error occurred, display an alert with the error message
       } else {
         alert(data.info);
         console.log("Error:", data.info);
@@ -246,24 +285,30 @@ function deleteMessage(button) {
 
 // ##############################
 // ADMIN_CLIPCARDS.HTML
+
+// Handles deleting clipcards from the admin interface.
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("delete-button")) {
+    // Retrieve the clipcard ID from the data-clipcard-id attribute of the clicked button
     var clipcardId = event.target.getAttribute("data-clipcard-id");
     deleteClipcard(clipcardId);
   }
 });
 
+// Send a DELETE request to the server to delete the clipcard
 function deleteClipcard(clipcardId) {
   fetch("/delete_clipcard/" + clipcardId, {
     method: "DELETE",
   })
     .then((response) => {
       if (response.ok) {
+        // Find the clipcard element in the DOM and remove it
         var clipcardElement = document.getElementById("clipcard_" + clipcardId);
         if (clipcardElement) {
           clipcardElement.remove();
         }
       } else {
+        // If the request fails, throw an error
         throw new Error("Kunne ikke slette klippekortet.");
       }
     })
@@ -273,10 +318,13 @@ function deleteClipcard(clipcardId) {
     });
 }
 
+// Handles form submission for tasks
 $(document).ready(function () {
   $("body").on("click", "#submitTaskButton", function () {
     console.log("Button clicked");
+    // Retrieve form data
     var formData = new FormData($("#taskForm")[0]);
+    // Send an AJAX request to submit the task
     $.ajax({
       url: "/submit_task",
       type: "POST",
@@ -284,11 +332,13 @@ $(document).ready(function () {
       processData: false,
       contentType: false,
       success: function (response) {
+        // Log and display success message
         console.log("Success response:", response);
         $("#taskSubmissionMessage").text(response.info).show();
         $("#taskForm")[0].reset();
       },
       error: function (xhr) {
+        // Log and display error message if there's an error in the request
         console.error("Error response:", xhr);
         var response = JSON.parse(xhr.responseText);
         alert("Der opstod en fejl: " + response.info);
@@ -299,11 +349,17 @@ $(document).ready(function () {
 
 // ##############################
 // PASSWORD_FIELD.TPL
+
+// Manages the visibility toggle for password fields.
 $(document).ready(function () {
+  // Attach a click event handler
   $("#visibility_button").on("click", function () {
+    // Retrieve the password input field and visibility icons
     var input = $("#password_input");
     var icons = $(".visibility_icon");
+    // Toggle the visibility of the password input field between text and password
     input.attr("type", input.attr("type") === "password" ? "text" : "password");
+    // Toggle the visibility of visibility icons
     icons.toggleClass("object_hidden");
   });
 });
