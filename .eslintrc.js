781e7ccc20a1e81f452be341231d7c1702424c5d
“isab554b” <isab554b@stud.kea.dk>
2024-06-12 11:43:55 +0200

    eslintrc.js updated 2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,18 +10,21 @@ module.exports = {
     sourceType: "module",
   },
   rules: {
-    "no-unused-vars": "warn", // Vis advarsler for uudnyttede variabler
-    indent: ["error", 2], // Vis fejl, hvis der ikke er to mellemrum for indrykning
-    quotes: ["error", "single"], // Vis fejl, hvis der ikke bruges enkeltcitationstegn
-    semi: ["error", "always"], // Vis fejl, hvis der mangler semikolon
-    "comma-dangle": ["error", "always-multiline"], // Vis fejl, hvis der mangler komma ved multiline statements
-    "no-trailing-spaces": "error", // Vis fejl, hvis der er trailing spaces
-    "comma-spacing": "error", // Vis fejl, hvis der er fejl i komma-spacing
-    "brace-style": "error", // Vis fejl, hvis der er fejl i brace style
-    "no-multi-spaces": "error", // Vis fejl, hvis der er multiple spaces
-    "arrow-spacing": "error", // Vis fejl, hvis der er fejl i arrow spacing
-    "no-multiple-empty-lines": "error", // Vis fejl, hvis der er multiple empty lines
-    "no-var": "error", // Vis fejl, hvis var bruges i stedet for let/const
-    "prefer-const": "error", // Vis fejl, hvis der bruges let i stedet for const, når variablen ikke ændres
+    "no-unused-vars": "warn",
+    indent: ["error", 2],
+    quotes: ["error", "single"],
+    semi: ["error", "always"],
+    "comma-dangle": ["error", "always-multiline"],
+    "no-trailing-spaces": "error",
+    "comma-spacing": "error",
+    "brace-style": "error",
+    "no-multi-spaces": "error",
+    "arrow-spacing": "error",
+    "no-multiple-empty-lines": "error",
+    "no-var": "error",
+    "prefer-const": "error",
+  },
+  globals: {
+    $: true,
   },
 };
