cd0ca461ce3c4c5286734883241ff083534afcb9
“isab554b” <isab554b@stud.kea.dk>
2024-06-12 11:27:21 +0200

    eslintrc.js added

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "no-console": "warn", // Vis advarsler, hvis der er konsoludskrivninger
+    "no-unused-vars": "warn", // Vis advarsler, hvis der er uudnyttede variabler
+    indent: ["error", 2], // Vis fejl, hvis der ikke er to mellemrum for indrykning
+    quotes: ["error", "single"], // Vis fejl, hvis der ikke bruges enkeltcitationstegn
+    semi: ["error", "always"], // Vis fejl, hvis der mangler semikolon
+    "comma-dangle": ["error", "always-multiline"], // Vis fejl, hvis der mangler komma ved multiline statements
+    "no-trailing-spaces": "error", // Vis fejl, hvis der er trailing spaces
+    "comma-spacing": "error", // Vis fejl, hvis der er fejl i komma-spacing
+    "brace-style": "error", // Vis fejl, hvis der er fejl i brace style
+    "no-multi-spaces": "error", // Vis fejl, hvis der er multiple spaces
+    "arrow-spacing": "error", // Vis fejl, hvis der er fejl i arrow spacing
+    "no-multiple-empty-lines": "error", // Vis fejl, hvis der er multiple empty lines
+    "no-var": "error", // Vis fejl, hvis var bruges i stedet for let/const
+    "prefer-const": "error", // Vis fejl, hvis der bruges let i stedet for const, når variablen ikke ændres
+    // Flere regler kan tilføjes her
+  },
+};
--- eslint.config.js
+++
@@ -1,8 +0,0 @@
-const globals = require("globals");
-const pluginJs = require("@eslint/js");
-
-export const eslintConfig = [
-  { files: ["static/**/*.js"], languageOptions: { sourceType: "module" } },
-  { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
-];
